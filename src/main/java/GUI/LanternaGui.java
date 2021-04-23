84ba997f0ab7ba3db099408e5e6ecb7d496cc900
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-04-23 15:30:24 +0100

    Basic GUI

---
+++ src/main/java/GUI/LanternaGui.java
@@ -0,0 +1,77 @@
+package GUI;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class LanternaGui implements GUI {
+    private final TerminalScreen screen;
+
+    public LanternaGui(int width, int height) throws IOException, FontFormatException, URISyntaxException {
+        AWTTerminalFontConfiguration fontConfig = loadSquareFont();
+        Terminal terminal = createTerminal(width, height, fontConfig);
+        screen = createScreen(terminal);
+    }
+
+    private TerminalScreen createScreen(Terminal terminal) throws IOException {
+        final TerminalScreen screen;
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);  // maybe we'll use mouse later
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        return screen;
+    }
+
+    private Terminal createTerminal(int width, int height, AWTTerminalFontConfiguration fontConfig) throws IOException {
+        TerminalSize terminalSize = new TerminalSize(width, height);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        terminalFactory.setForceAWTOverSwing(true);
+        terminalFactory.setTerminalEmulatorFontConfiguration(fontConfig);
+        Terminal terminal = terminalFactory.createTerminal();
+        return terminal;
+    }
+
+    private AWTTerminalFontConfiguration loadSquareFont() throws URISyntaxException, FontFormatException, IOException {
+        URL resource = getClass().getClassLoader().getResource("square.ttf");
+        File fontFile = new File(resource.toURI());
+        Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(font);
+
+        Font loadedFont = font.deriveFont(Font.PLAIN, 15);
+        AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
+        return fontConfig;
+    }
+
+    @Override
+    public void drawCharacter(int x, int y, char c) {
+        screen.setCharacter(x, y, TextCharacter.fromCharacter(c)[0]);
+    }
+
+    @Override
+    public void clear() {
+        screen.clear();
+    }
+
+    @Override
+    public void refresh() throws IOException {
+        screen.refresh();
+    }
+
+    @Override
+    public void close() throws IOException {
+        screen.close();
+    }
+}
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,13 @@
+import GUI.LanternaGui;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Game {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FontFormatException, IOException, URISyntaxException {
+        LanternaGui gui = new LanternaGui(150, 100);
         System.out.println("Here we go");
     }
 }
