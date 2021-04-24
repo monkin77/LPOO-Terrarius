331b408f279920c30dbe504451b189f7e22d9ede
up201906682 <up201906682@fe.up.pt>
2021-04-24 16:25:56 +0100

    Created Arena builder and changed Game.java structure

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,8 @@
+import GUI.GUI;
 import GUI.LanternaGui;
 import Model.Position;
 import Model.arena.Arena;
+import Model.arena.DefaultArenaBuilder;
 import Model.elements.Hero;
 
 import java.awt.*;
@@ -8,12 +10,19 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Game {
+    private final GUI gui;
+    private final Arena arena;
 
     public static void main(String[] args) throws FontFormatException, IOException, URISyntaxException {
-        LanternaGui gui = new LanternaGui(150, 100);
         System.out.println("Here we go");
 
-        Arena arena = new Arena(150, 100);
-        arena.setHero(new Hero(new Position(15, 15)));  /* Use arena builder in the future */
+        Game game = new Game(150, 100);
+    }
+
+    public Game(int width, int height) throws FontFormatException, IOException, URISyntaxException {
+        this.gui = new LanternaGui(width, height);
+        System.out.println("Here we go");
+
+        this.arena = new DefaultArenaBuilder(width, height).createArena();
     }
 }
