fafc470ac926074a1a30e95b54d57483ffd4f0b0
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-04-30 20:02:08 +0100

    Added input reading

--- src/main/java/GUI/LanternaGui.java
+++ src/main/java/GUI/LanternaGui.java
@@ -1,16 +1,19 @@
 package GUI;
 
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.DoublePrintingTextGraphics;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -21,12 +24,26 @@ import static Viewer.ViewerConstants.DEFAULT_FOREGROUND_COLOR;
 public class LanternaGui implements GUI {
     private final TerminalScreen screen;
     private TextGraphics graphics;
+    private boolean mouseClicked;
 
     public LanternaGui(int width, int height) throws IOException, FontFormatException, URISyntaxException {
         AWTTerminalFontConfiguration fontConfig = loadSquareFont();
         Terminal terminal = createTerminal(width, height, fontConfig);
+        addMouseListener(terminal);
+
         screen = createScreen(terminal);
         graphics = screen.newTextGraphics();
+    }
+
+    private void addMouseListener(Terminal terminal) {
+        mouseClicked = false;
+        ((AWTTerminalFrame) terminal).getComponent(0).addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // If needed, use e.getX() and e.getY() to get the position of the click
+                mouseClicked = true;
+            }
+        });
     }
 
     private TerminalScreen createScreen(Terminal terminal) throws IOException {
@@ -92,6 +109,21 @@ public class LanternaGui implements GUI {
 
     @Override
     public ACTION getNextAction() throws IOException {
+        if (mouseClicked) {
+            mouseClicked = false;
+            return ACTION.CLICK;
+        }
+
+        KeyStroke keyStroke = screen.readInput();
+
+        if (keyStroke.getKeyType() == KeyType.EOF ||
+            keyStroke.getKeyType() == KeyType.Escape) return ACTION.QUIT;
+
+        if (keyStroke.getKeyType() == KeyType.ArrowUp) return ACTION.UP;
+        if (keyStroke.getKeyType() == KeyType.ArrowRight) return ACTION.RIGHT;
+        if (keyStroke.getKeyType() == KeyType.ArrowDown) return ACTION.DOWN;
+        if (keyStroke.getKeyType() == KeyType.ArrowLeft) return ACTION.LEFT;
+
         return ACTION.NONE;
     }
 }
