e4c12d3c0e20c6db93dcdb8eeb6ecbd6854c3522
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-04-30 19:30:55 +0100

    Hero Controller

---
+++ src/main/java/Controller/HeroController.java
@@ -0,0 +1,57 @@
+package Controller;
+
+import GUI.GUI;
+import Model.Position;
+import Model.arena.Arena;
+
+public class HeroController {
+    private final Arena arena;
+
+    public HeroController(Arena arena) {
+        this.arena = arena;
+    }
+
+    private void moveHero(Position position) {
+        // Can't move outside of arena boundaries
+        if (position.getX() < 0 || position.getX() > arena.getWidth()
+            || position.getY() < 0 || position.getY() > arena.getHeight())
+            return;
+
+        if (arena.isEmpty(position)) {
+            arena.getHero().setPosition(position);
+        }
+    }
+
+    public void moveHeroLeft() {
+        moveHero(arena.getHero().getPosition().getLeft());
+    }
+
+    public void moveHeroRight() {
+        moveHero(arena.getHero().getPosition().getRight());
+    }
+
+    public void moveHeroUp() {
+        moveHero(arena.getHero().getPosition().getUp());
+    }
+
+    public void moveHeroDown() {
+        moveHero(arena.getHero().getPosition().getDown());
+    }
+
+    public void doAction(GUI.ACTION action) {
+        switch (action) {
+            case UP:
+                // TO DO: JUMPING
+                break;
+            case LEFT:
+                moveHeroLeft();
+                break;
+            case RIGHT:
+                moveHeroRight();
+                break;
+            case CLICK:
+                // TO DO: USE ITEM
+                break;
+        }
+    }
+}
