9725f861c319ad46d63ebd5a7743b7c179de54b2
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-05-07 14:34:26 +0100

    Changed Hero Tests

--- src/test/java/Model/elements/HeroTest.java
+++ src/test/java/Model/elements/HeroTest.java
@@ -5,7 +5,6 @@ import Model.Position;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 public class HeroTest {
     Hero hero;
@@ -16,26 +15,14 @@ public class HeroTest {
     }
 
     @Test
-    public void level() {
+    public void startLevel() {
         Level heroLevel = hero.getLevel();
         Assertions.assertEquals(heroLevel.getNumLevel(), 1);
         Assertions.assertEquals(heroLevel.getCurrentXP(), 0);
-
-        Level newLevel = Mockito.mock(Level.class);
-        Mockito.when(newLevel.getNumLevel()).thenReturn(3);
-        Mockito.when(newLevel.getCurrentXP()).thenReturn(250L);
-
-        hero.setLevel(newLevel);
-        heroLevel = hero.getLevel();
-        Assertions.assertEquals(heroLevel.getNumLevel(), 3);
-        Assertions.assertEquals(heroLevel.getCurrentXP(), 250L);
     }
 
     @Test
-    public void health() {
+    public void startHealth() {
         Assertions.assertEquals(100, hero.getHealth());
-
-        hero.setHealth(90);
-        Assertions.assertEquals(90, hero.getHealth());
     }
 }
