0d2396ebf37f5c487c706e4094ab838c9bfd315f
up201905405 <up201905405@fe.up.pt>
2021-05-30 12:05:45 +0100

    Better itemShopViewer

--- src/main/java/Terrarius/Viewer/Viewer.java
+++ src/main/java/Terrarius/Viewer/Viewer.java
@@ -3,7 +3,8 @@ package Terrarius.Viewer;
 import Terrarius.GUI.GUI;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public abstract class Viewer<T> {
-    public abstract void draw(GUI gui, T model) throws IOException;
+    public abstract void draw(GUI gui, T model) throws IOException, URISyntaxException;
 }
