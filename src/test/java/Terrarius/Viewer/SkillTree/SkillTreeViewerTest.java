b675d758033aa734b21961edf874e4b61a5dce5c
up201906682 <up201906682@fe.up.pt>
2021-05-30 22:11:11 +0100

    Finished merging

--- src/test/java/Terrarius/Viewer/SkillTree/SkillTreeViewerTest.java
+++ src/test/java/Terrarius/Viewer/SkillTree/SkillTreeViewerTest.java
@@ -1,72 +1,17 @@
 package Terrarius.Viewer.SkillTree;
 
 import Terrarius.GUI.GUI;
-<<<<<<< HEAD
-import Terrarius.GUI.LanternaGui;
 import Terrarius.Model.Game.Position;
-import Terrarius.Model.Game.elements.Element;
 import Terrarius.Model.Game.elements.hero.HeroStats;
 import Terrarius.Model.SkillTree.SkillTree;
-import Terrarius.Utils.Dimensions;
-import Terrarius.Viewer.Image.ColoredImage;
 import org.junit.jupiter.api.Assertions;
-=======
 import Terrarius.Model.Game.Level;
-import Terrarius.Model.Game.Position;
-import Terrarius.Model.Game.elements.hero.HeroStats;
-import Terrarius.Model.SkillTree.SkillTree;
->>>>>>> master
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.io.IOException;
 
-<<<<<<< HEAD
-class SkillTreeViewerTest {
-    private SkillTreeViewer skillTreeViewer;
-    private GUI gui;
-    private SkillTree skillTree;
-    private ColoredImage image;
-
-    @BeforeEach
-    public void setup() {
-        this.skillTreeViewer = new SkillTreeViewer();
-
-        this.gui = Mockito.mock(LanternaGui.class);
-        this.skillTree = Mockito.mock(SkillTree.class);
-        this.image = Mockito.spy(this.skillTreeViewer.getImage());
-        this.skillTreeViewer.setImage(this.image);
-
-        Mockito.when(skillTree.getHeroStats()).thenReturn(new HeroStats());
-        Mockito.when(skillTree.getSelectedIndex()).thenReturn(1);
-        Mockito.when(skillTree.getUsedPoints()).thenReturn(5);
-    }
-
-    @Test
-    public void validateImage() {
-        Assertions.assertEquals(new Dimensions(50, 50), this.skillTreeViewer.getImage().getDimensions());
-    }
-
-    @Test
-    public void draw() throws IOException {
-        this.skillTreeViewer.draw(gui, skillTree);
-
-        Position treePos = new Position(SkillTreeViewerConstants.START_POS_X, SkillTreeViewerConstants.START_POS_Y);
-        Mockito.verify(this.image, Mockito.times(1)).draw(treePos, Element.Orientation.RIGHT, gui);
-    }
-
-    @Test
-    public void shouldUpdate() throws IOException {
-        Assertions.assertTrue(skillTreeViewer.needsUpdate(skillTree));
-
-        skillTreeViewer.draw(gui, skillTree);
-
-        Assertions.assertFalse(skillTreeViewer.needsUpdate(skillTree));
-
-    }
-}
-=======
 public class SkillTreeViewerTest {
     private GUI gui;
     private SkillTree skillTree;
@@ -107,5 +52,14 @@ public class SkillTreeViewerTest {
         Mockito.verify(gui, Mockito.times(152)).drawCharacter(Mockito.anyInt(), Mockito.anyInt(),
                 Mockito.anyChar(), Mockito.anyString(), Mockito.anyString());
     }
+
+    @Test
+    public void shouldUpdate() throws IOException {
+        Assertions.assertTrue(viewer.needsUpdate(skillTree));
+
+        viewer.draw(gui, skillTree);
+
+        Assertions.assertFalse(viewer.needsUpdate(skillTree));
+
+    }
 }
->>>>>>> master
