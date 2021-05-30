1411fc5dc04532fd0e5f589f360690ab23f9177e
up201906682 <up201906682@fe.up.pt>
2021-05-30 16:43:52 +0100

    Added MenuStateTest

---
+++ src/test/java/Terrarius/States/MenuStateTest.java
@@ -0,0 +1,29 @@
+package Terrarius.States;
+
+import Terrarius.Controller.Menu.MenuController;
+import Terrarius.GUI.GUI;
+import Terrarius.Model.Menu.Menu;
+import Terrarius.Viewer.Menu.MenuViewer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class MenuStateTest {
+    private State state;
+    private GUI gui;
+    private Menu menu;
+
+    @BeforeEach
+    public void setup() {
+        this.menu = Mockito.mock(Menu.class);
+        this.state = new MenuState(this.menu);
+    }
+
+    @Test
+    public void menuSpecifiers() {
+        Assertions.assertTrue(state.getModel() instanceof Menu);
+        Assertions.assertTrue(state.getController() instanceof MenuController);
+        Assertions.assertTrue(state.getViewer() instanceof MenuViewer);
+    }
+}
